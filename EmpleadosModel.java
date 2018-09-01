69b30cd7cb79e196e1a5f01effbe4dc7ad29a011
Alexander Roque <roquealexander7@gmail.com>
2018-08-31 23:35:12 -0600

    Ultimo cambio, Luis nos bendiga.

---
+++ EmpleadosModel.java
@@ -0,0 +1,225 @@
+package sv.edu.udb.www.model;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import sv.edu.udb.www.beans.Cliente;
+import sv.edu.udb.www.beans.Cupon;
+import sv.edu.udb.www.beans.EstadoCupon;
+import sv.edu.udb.www.beans.Empleado;
+import sv.edu.udb.www.beans.Oferta;
+import sv.edu.udb.www.beans.Usuario;
+import static sv.edu.udb.www.model.Conexion.conexion;
+
+public class EmpleadosModel extends Conexion {
+
+    /*public Cupon obtenerCupon(String codigo) throws SQLException {
+        try {
+
+            Cliente cliente = new Cliente();
+            Oferta oferta = new Oferta();
+
+            String sql = "SELECT * FROM cupones AS cup INNER JOIN clientes AS cli ON cup.IdCliente=cli.IdCliente INNER JOIN ofertas AS ofer ON cup.IdOferta=ofer.IdOferta WHERE CodigoCupo = ?";
+            this.conectar();
+            st = conexion.prepareStatement(sql);
+            st.setString(1, codigo);
+            rs = st.executeQuery();
+
+            if (rs.next()) {
+                Cupon cupon = new Cupon();
+                cupon.setCodigoCupo(rs.getString("CodigoCupo"));
+                cupon.setFechaCompra(rs.getDate("FechaCompra"));
+                cupon.setFechaCanje(rs.getDate("FechaCanje"));
+                //cupon.setCliente((Cliente) rs.getObject("IdCliente"));
+                //cupon.setOferta((Oferta) rs.getObject("IdOferta"));
+
+                this.desconectar();
+                return cupon;
+            }
+            this.desconectar();
+            return null;
+        } catch (SQLException ex) {
+            Logger.getLogger(EmpleadosModel.class.getName()).log(Level.SEVERE, null, ex);
+            this.desconectar();
+            return null;
+        }
+    }*/
+    public List<Cupon> obtenerCupon(String codigo) throws SQLException {
+
+        try {
+            List<Cupon> lista = new ArrayList<>();
+            String sql = "SELECT * FROM cupones AS cup INNER JOIN clientes AS cli ON cup.IdCliente=cli.IdCliente INNER JOIN ofertas AS ofer ON cup.IdOferta=ofer.IdOferta INNER JOIN estadocupon AS estaCu ON cup.IdEstadoCupon=estaCu.IdEstadoCupon INNER JOIN empresas AS emp ON ofer.CodigoEmpresa=emp.CodigoEmpresa WHERE CodigoCupo = ?";
+            this.conectar();
+            st = conexion.prepareStatement(sql);
+            st.setString(1, codigo);
+            rs = st.executeQuery();
+
+            while (rs.next()) {
+                Cupon cupon = new Cupon();
+                cupon.setCodigoCupo(rs.getString("CodigoCupo"));
+                cupon.setFechaCompra(rs.getString("FechaCompra"));
+                cupon.setFechaCanje(rs.getString("FechaCanje"));
+                cupon.setCliente(new Cliente(rs.getString("NombreClientes"), rs.getString("ApellidosClientes"), rs.getString("DUI")));
+                cupon.setOferta(new Oferta(rs.getString("TituloOferta")));
+                cupon.setEstadoCupon(new EstadoCupon(rs.getString("Estado")));
+                lista.add(cupon);
+            }
+            this.desconectar();
+            return lista;
+        } catch (SQLException ex) {
+            Logger.getLogger(EmpleadosModel.class.getName()).log(Level.SEVERE, null, ex);
+            this.desconectar();
+            return null;
+        }
+    }
+
+    //Prueba
+    public Cupon obtenerC(String codigo) throws SQLException {
+        try {
+            String sql = "SELECT * FROM cupones WHERE CodigoCupo = ?";
+            this.conectar();
+            st = conexion.prepareStatement(sql);
+            st.setString(1, codigo);
+            rs = st.executeQuery();
+            
+            if (rs.next()) {
+                Cupon cupon = new Cupon();
+                
+                cupon.setCodigoCupo(rs.getString("CodigoCupo"));
+                
+                this.desconectar();
+                return cupon;
+            }
+            this.desconectar();
+            return null;
+        } catch (SQLException ex) {
+            Logger.getLogger(EmpleadosModel.class.getName()).log(Level.SEVERE, null, ex);
+            this.desconectar();
+            return null;
+        }
+    }
+
+    public int canjearCupon(String codigo) throws SQLException {
+        try {
+            int filasAfectadas = 0;
+            String sql = "UPDATE cupones SET IdEstadoCupon=2 WHERE CodigoCupo = ?";
+            this.conectar();
+            st = conexion.prepareStatement(sql);
+            st.setString(1, codigo);
+
+            filasAfectadas = st.executeUpdate();
+            this.desconectar();
+            return filasAfectadas;
+        } catch (SQLException ex) {
+            Logger.getLogger(EmpleadosModel.class.getName()).log(Level.SEVERE, null, ex);
+            this.desconectar();
+            return 0;
+        }
+    }
+
+    public List<Empleado> listarEmpleados(String correo) throws SQLException {
+        try {
+            List<Empleado> lista = new ArrayList<>();
+
+            String sql = "Select e.CodigoEmpresa from empresas e inner join usuarios u on e.IdUsuario=u.IdUsuario where u.correo=?";
+            this.conectar();
+            st = conexion.prepareStatement(sql);
+            st.setString(1, correo);
+            rs = st.executeQuery();
+            rs.next();
+
+            String CodigoEmpresa = rs.getString("CodigoEmpresa");
+
+            sql = "Select * from empleado e inner join empresas em on e.CodigoEmpresa=em.CodigoEmpresa"
+                    + " inner join usuarios u on e.IdUsuario=u.IdUsuario where em.CodigoEmpresa=?";
+            st = conexion.prepareStatement(sql);
+            st.setString(1, CodigoEmpresa);
+
+            rs = st.executeQuery();
+            while (rs.next()) {
+                Empleado empleado = new Empleado();
+                empleado.setIdEmpleado(rs.getInt("IdEmpleado"));
+                empleado.setNombreEmpleado(rs.getString("NombreEmpleado"));
+                empleado.setApellidoEmpleado(rs.getString("ApellidoEmpleado"));
+                empleado.setUsuario(new Usuario(rs.getString("correo")));
+                lista.add(empleado);
+            }
+            this.desconectar();
+            return lista;
+        } catch (SQLException ex) {
+            Logger.getLogger(OfertasModel.class.getName()).log(Level.SEVERE, null, ex);
+            this.desconectar();
+            return null;
+        }
+    }
+
+    public int insertarEmpleado(Empleado empleado, String correo, int idUsuario) throws SQLException {
+        try {
+            int filasAfectadas = 0;
+            String sql = "select * from empresas e inner join usuarios u on e.IdUsuario=u.IdUsuario where u.correo=?";
+            this.conectar();
+            st = conexion.prepareStatement(sql);
+            st.setString(1, correo);
+            rs = st.executeQuery();
+            rs.next();
+            String codigoEmpresa = rs.getString("CodigoEmpresa");
+
+            sql = "Insert into empleado values(NULL,?,?,?,?)";
+            st = conexion.prepareStatement(sql);
+            st.setString(1, empleado.getNombreEmpleado());
+            st.setString(2, empleado.getApellidoEmpleado());
+            st.setInt(3, idUsuario);
+            st.setString(4, codigoEmpresa);
+            filasAfectadas = st.executeUpdate();
+            this.desconectar();
+            return filasAfectadas;
+        } catch (SQLException ex) {
+            Logger.getLogger(EmpleadosModel.class.getName()).log(Level.SEVERE, null, ex);
+            this.desconectar();
+            return 0;
+        }
+    }
+
+    public int eliminarEmpleado(int idEmpleado) throws SQLException {
+        int IdUsuario = 0;
+        int filasAfectadas = 0;
+        try {
+            String sql = "SELECT * from empleado where IdEmpleado=?";
+
+            this.conectar();
+            st = conexion.prepareStatement(sql);
+            st.setInt(1, idEmpleado);
+            rs = st.executeQuery();
+
+            if (rs.next()) {
+                IdUsuario = rs.getInt("IdUsuario");
+            } else {
+                this.desconectar();
+                return 0;
+            }
+
+            sql = "Delete FROM empleado WHERE IdEmpleado=?";
+            st = conexion.prepareStatement(sql);
+            st.setInt(1, idEmpleado);
+            filasAfectadas = st.executeUpdate();
+            if (filasAfectadas == 0) {
+                this.desconectar();
+                return 0;
+            }
+
+            sql = "Delete From usuarios where IdUsuario=?";
+            st = conexion.prepareStatement(sql);
+            st.setInt(1, IdUsuario);
+            filasAfectadas = st.executeUpdate();
+
+            return filasAfectadas;
+        } catch (SQLException ex) {
+            Logger.getLogger(EmpleadosModel.class.getName()).log(Level.SEVERE, null, ex);
+            this.desconectar();
+            return 0;
+        }
+
+    }
+}
---
+++ canjearCupon.jsp
@@ -0,0 +1,88 @@
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Canjear cupón</title>
+        <jsp:include page="/Empleado/head.jsp" />
+    </head>
+    <body>
+        <jsp:include page="/Empleado/menuEmpleado.jsp" />
+        <div id="formularioCanje">
+            <div class="container-fluid">
+                <div class="row">
+                    <h3 class="h3">Canjear cupón</h3>
+                </div>
+                <div class="row">
+                    <div class="col-md-7">
+                        <!--<div class="well well-sm"><strong><span class="glyphicon glyphicon-asterisk"></span>Campos requeridos</strong></div>-->
+                        <form  role="form" action="${pageContext.request.contextPath}/empleados.do" method="POST">
+                            <input type="hidden" name="operacion" value="obtener" />
+                            <div class="form-group" id="codigoCupon">
+                                <div class="form-group">
+                                    <input type="text" class="form-control col-md-7" name="codigo" id="codigo" value="" placeholder="Ingresa el código del cupón" >
+                                </div>
+                                <input type="submit" class="btn btn-info" id="verificar" value="Verificar" name="Verificar">    
+                            </div><br>
+                        </form>
+
+                        <form role="form" action="${pageContext.request.contextPath}/empleados.do" method="POST">    
+                            <div class="form-group">
+                                <c:forEach items="${requestScope.listaCupones}" var="cupones">
+                                    <div class="informacionCupon">
+                                        <input type="hidden" name="operacion" value="canjear" />
+                                        <label for="nombre">Código cupón: ${cupones.codigoCupo}</label><br><br>
+                                        <input type="hidden" name="codigoCupon" value="${cupones.codigoCupo}" />
+                                        <label for="estadoCupon">Estado: ${cupones.estadoCupon.estado}</label>
+                                        <input type="hidden" name="estadoCupon" value="${cupones.estadoCupon.estado}" />
+                                        <label for="fechaCompra">Fecha de compra: ${cupones.fechaCompra}</label><div class="fechaC"><label for="fechaCanje" class="fechaCanje">Fecha canje: ${cupones.fechaCanje}</label></div>                                      
+                                        <hr class="separador" />
+                                    </div>
+                                    <div class="informacionCliente">
+                                        <!--<label for="duiCliente">DUI:</label><br>-->
+                                        <input type="hidden" name="duiComprador" value="${cupones.cliente.dui}" />
+                                        <label for="nombreCliente">Cliente: ${cupones.cliente.nombreClientes} ${cupones.cliente.apellidosClientes}</label>
+                                        <input type="text" class="form-control col-md-7" name="duiCanjeador" id="duiCanjeador" placeholder="Ingresa el DUI del canjeador"><br>
+                                        <hr class="separador" />
+                                    </div>
+                                    <div class="informacionOferta">
+                                        <label for="tituloOferta">Titulo oferta: ${cupones.oferta.tituloOferta}</label>
+                                    </div>                               
+                                </c:forEach>
+                            </div><br>
+
+                            <div id="botones">
+                                <input type="submit" class="btn btn-success canje" value="Canjear" name="Canjear">
+                                <a class="btn btn-danger cancelar" href="${pageContext.request.contextPath}/empleados.do?operacion=canje">Cancelar</a>
+                            </div>
+                        </form>
+                    </div>
+                </div> 
+            </div>
+            <script>
+                <c:if test="${not empty Obtenido}">
+                document.getElementById('botones').style.display = "block";
+                document.getElementById('formularioCanje').style = "margin-top: 0.8%; height: 626px;";
+                </c:if>
+                <c:if test="${not empty Exito}">
+                document.getElementById('botones').style.display = "block";
+                document.getElementById('formularioCanje').style = "margin-top: 0.8%; height: 626px;";
+                alertify.success('${Exito}');
+                    <c:set var="Exito" value="" scope="session"></c:set>
+                </c:if>
+                <c:if test="${not empty Fracaso}">
+                document.getElementById('botones').style.display = "none";
+                document.getElementById('formularioCanje').style = "margin-top: 8%;";
+                alertify.error('${Fracaso}');
+                    <c:set var="Fracaso" value="" scope="session"></c:set>
+                </c:if>
+                <c:if test="${not empty Fracaso1}">
+                document.getElementById('botones').style.display = "none";
+                document.getElementById('formularioCanje').style = "margin-top: 0.8%; height: 626px;";
+                alertify.error('${Fracaso1}');
+                    <c:set var="Fracaso" value="" scope="session"></c:set>
+                </c:if>
+            </script>                
+    </body>
+</html>
